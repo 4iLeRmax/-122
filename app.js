54ce36c662b0f3dfcd5d9aad8c7e9d2682d00be9
4iLeRmax <102809824+4iLeRmax@users.noreply.github.com>
2022-08-30 18:43:16 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -96,3 +96,25 @@ let menuBtn = document.querySelector('.burger__menu');
 menuBtn.addEventListener('click', ()=>{
   nav.classList.toggle('active');
 });
+
+
+let checkbox = document.querySelector('input[name=theme]');
+
+checkbox.addEventListener('change', function() {
+    if(this.checked) {
+        trans()
+        document.documentElement.setAttribute('data-theme', 'light')
+    } else {
+        trans()
+        document.documentElement.setAttribute('data-theme', 'dark')
+    }
+});
+
+
+
+let trans = () => {
+    document.documentElement.classList.add('transition');
+    window.setTimeout(() => {
+        document.documentElement.classList.remove('transition')
+    }, 1000)
+}
